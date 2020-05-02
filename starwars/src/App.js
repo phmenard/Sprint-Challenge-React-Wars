4cba77317028ee9cea02422527a5e5962038cfac
unknown <clmonitor2013@gmail.com>
2020-05-01 21:13:45 -0400

    added characters

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
+import CharacterContainer from './components/Character' ;
+
 import './App.css';
 
 const App = () => {
@@ -9,9 +13,28 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  // set a state for the data
+  const [characters, setCharacters] = useState([]);
+
+
+  // Grab rick and marty data
+  useEffect(() => {  
+    axios
+        .get(`https://rickandmortyapi.com/api/character/?page=1`)
+        .then(response => {
+            console.log(response.data.results);
+            setCharacters(response.data.results);
+        })
+        .catch(err => {
+            console.log(err);
+        }
+        );
+}, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <CharacterContainer characters={characters}/>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,43 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import './character.css';
+
+import {
+    Card, Button, CardImg, CardTitle, CardText, CardDeck,
+    CardSubtitle, CardBody
+} from 'reactstrap';
+
+function CharacterContainer(props) {
+
+    return (
+        <div className="deckContainer">
+            <section className='charSection'>
+        
+            {props.characters.map((character, i) => {
+                return <CharacterCard character={character} />;
+            })}
+        
+            </section>
+        </div>
+    );
+
+
+}
+
+function CharacterCard(props) {
+    console.log(props);
+
+    return (
+        <Card className="card">
+            <CardImg className="img" top width="100%" src={props.character.image} alt="Card image cap" />
+            <CardBody>
+                <CardTitle>{props.character.name}</CardTitle>
+                <CardSubtitle>{props.character.gender}</CardSubtitle>
+                <CardText></CardText>
+                <Button></Button>
+            </CardBody>
+        </Card>
+    );
+}
+
+export default CharacterContainer;
